4bf7aa01dceda874406003b9e1e3e0a100de0b32
RadikAkhme <a.radik.s@ya.ru>
2022-08-21 23:56:28 +0300

    Add functional.

--- Pomodoro/Sources/App/ViewController.swift
+++ Pomodoro/Sources/App/ViewController.swift
@@ -11,9 +11,13 @@ import SnapKit
 class ViewController: UIViewController {
     
     // MARK: - Properties
-
-    let shape = CAShapeLayer()
+        
+    var circularViewDuration: TimeInterval = 5
+    var isStarted = false
+    var isWorkTime = false
+    var timer = Timer()
     var durationTimer = 5
+    let shape = CAShapeLayer()
 
     // MARK: - Outlets
         
@@ -71,8 +75,22 @@ class ViewController: UIViewController {
     // MARK: - Actions
 
     @objc private func playPauseButtonPressed() {
-       
+        basicAnimation()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
+        timer.tolerance = 0.1
+        playPauseButton.setImage(UIImage(systemName: "pause"), for: .normal)
     }
+    
+    @objc private func timerAction() {
+        if durationTimer != 0 {
+            durationTimer -= 1
+            label.text = "\(durationTimer)"
+        } else {
+            timer.invalidate()
+        }
+    }
+    
+    // MARK: - Animation
     
     func createCircularPath() {
         let circlePath = UIBezierPath(arcCenter: view.center, radius: 150, startAngle: -(.pi / 2), endAngle: .pi * 2, clockwise: true)
@@ -92,6 +110,13 @@ class ViewController: UIViewController {
         shape.strokeEnd = 0
         view.layer.addSublayer(shape)
     }
-
-}
     
+    func basicAnimation() {
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation.duration = CFTimeInterval(durationTimer)
+        basicAnimation.toValue = 1
+        basicAnimation.fillMode = .forwards
+        basicAnimation.isRemovedOnCompletion = false
+        shape.add(basicAnimation, forKey: "animation")
+    }
+}
